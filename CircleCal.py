7331a519fcc14a4fa572d66c2b5cf489079bb782
Sweetuna <daniel780608@gmail.com>
2026-03-18 13:39:48 +0900

    2주차 과제

---
+++ CircleCal.py
@@ -0,0 +1,7 @@
+# 입력을 받습니다.
+r = float(input("반지름 입력> "))
+pi = float(3.14)
+
+# 출력합니다.
+print("둘레 결과:", r * 2 * pi)
+print("넓이 결과", r ** r * pi)
---
+++ kgToPound.py
@@ -0,0 +1,5 @@
+# 입력을 받습니다.
+kg = float(input("kg 입력> "))
+
+# 출력합니다.
+print("pound 결과:", kg * 2.20462)
